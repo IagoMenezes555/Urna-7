c7f05bcf90556ee5785abae19b32071883059b76
Iago <iagomenezes555@gmail.com>
2025-07-03 20:10:40 -0300

    add: new candidate list

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
-
+  novoCandidato = {nome: "", partido: "", numero: ""}
 }
